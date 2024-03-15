d4c042b9cf013bd5f83fc7e7d8f2465903aee506
iietmoon <otmannj56@gmail.com>
2024-03-15 10:22:45 +0000

    feat: linking bootstrap 5.0.2 as vendor for css, js

---
+++ footer.php
@@ -0,0 +1,5 @@
+
+    <!-- Bootstrap 5.0.2 Bundle with Popper -->
+    <script src="<?= get_template_directory_uri() . '/assets/vendors/bootstrap-5.0.2/js/bootstrap.bundle.min.js' ?>" ></script>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap 5.0.2 CSS -->
+    <link rel="stylesheet" href="<?= get_template_directory_uri() . '/assets/vendors/bootstrap-5.0.2/css/bootstrap.min.css' ?>" />
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
