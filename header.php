62ca1cae2d039b4853b096f9ea02a37e8b15ce99
iietmoon <otmannj56@gmail.com>
2024-03-15 10:42:54 +0000

    feat: linking bootstrap icons 5 and animate.css as intergated vendors

--- header.php
+++ header.php
@@ -7,8 +7,10 @@
 
     <!-- Bootstrap 5.0.2 CSS -->
     <link rel="stylesheet" href="<?= get_template_directory_uri() . '/assets/vendors/bootstrap-5.0.2/css/bootstrap.min.css' ?>" />
+    <link rel="stylesheet" href="<?= get_template_directory_uri() . '/assets/vendors/bootstrap-icons-1.11.3/font/bootstrap-icons.min.css' ?>" />
+    <link rel="stylesheet" href="<?= get_template_directory_uri() . '/assets/vendors/animate/animate.min.css' ?>" />
 
-    <title>Hello, world!</title>
+    <title><?= $brandablanca_meta_title ? $brandablanca_meta_title : bloginfo('name') . ' - ' . bloginfo('description'); ?></title>
+    <?php wp_head(); ?>
   </head>
-  <body>
-    <h1>Hello, world!</h1>
+  <body <?php body_class(); ?>>
